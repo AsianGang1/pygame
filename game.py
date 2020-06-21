e60f470b8c7585658f8bd8ee238b8abcf3684c45
James Huang <59267386+AsianGang1@users.noreply.github.com>
2020-06-20 20:53:06 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,18 @@
+"""turn 1:
+virus player gets to infect 1 guy
+he also has a shop to invest in more ppl to infect per turn
+he can also invest pts into getting people to not show symtoms
+he'll get points when a guy doesnt get caught and isolated with the infection
+next turn:
+medic player:
+has hospital to invest in more meds per turn:
+cna also invest in vaccine time
+so vaccine is created in 50 turns
+u can spend 3 meds to lower the turn count
+can also invest in capcity for hospital
+hospital has threashold
+if infections surpass the game ends
+and virus man wins"""
 from tkinkter import *
 root=Tk()
 canvas=Canvas(root,width=,height=)
