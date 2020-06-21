d7ebb2bdfb4335879bf5572f8ef13fc2c2943425
James Huang <59267386+AsianGang1@users.noreply.github.com>
2020-06-20 21:39:36 -0700

    added people object

--- game.py
+++ game.py
@@ -5,10 +5,10 @@ virus player abilities:
   invest in infections per turn (auto infect). each infected person will spread to 1 person next to them which can be increased to 4
   invest in manual infections where he can choose (manual infect). can increase manual infections from 1 up to 4. *supposed to simulate transportation*
   spends virus points for investments which are gained every turn for each infected individual which isn't caught by the medic
-medic player abilities: 
+medic player abilities:
   invest in more test kids per turn.
   look at people with high symptoms (takes 3 turns to show by default) and isolate them with the cost of 1 med kit
-  invest in hospital capacity so that there can be more infected people before the game is over 
+  invest in hospital capacity so that there can be more infected people before the game is over
   invest in vaccine speed so that the turns before winning as medic is reduced by 1 turn (starts at 50)
   invest in social distancing: the next turn most people are separated by 1 grid unit to prevent spread (doesn't work if there are too many people)
 winning as medic:
@@ -18,21 +18,34 @@ winning as virus:
 *every turn the people on the board will move 1 square unless social distancing is used that turn
 """
 from tkinter import *
-x=0
-y=0
-def getcoordonclick(eventorigin):
-   global x
-   global y
-   x=eventorigin.x
-   y=eventorigin.y
-   print(x,y)
+import random
+import time
 
-
-root=Tk()
-canvas=Canvas(root,width=700,height=500)
+root = Tk()
+canvas = Canvas(root, width=800, height=700)
 canvas.pack()
-for i in range(0,5600):
-  canvas.create_rectangle(50*(i%80)+1,50*(i//80)+1,50*(i%80)+51,50*(i//80)+51,fill="white",width=1)
+turn = "virus"
+people = []
+
+class Person:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.infected = False
+        self.turns_symptoms = 3
+        self.turns_death = 3 + random.randrange(1, 3)
+
+    def draw(self):
+        canvas.create_rectangle(self.x, self.y, self.x + 50, self.y + 50, fill="black")
+
+
+for i in range(0, 5600):
+    canvas.create_rectangle(50 * (i % 80), 50 * (i // 80), 50 * (i % 80) + 50, 50 * (i // 80) + 50, fill="white")
+for x in range(0, 30):
+    Person(x, x)
 canvas.update()
-canvas.bind("<Button-1>",getcoordonclick)
-mainloop()
+while True:
+    for person in people:
+        person.draw()
+    root.update()
+    time.sleep(0.01)
