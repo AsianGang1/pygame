0fab5fe5398438169bd6d530c8d9899900c74c9d
James Huang <59267386+AsianGang1@users.noreply.github.com>
2020-06-20 21:22:23 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,10 +1,21 @@
 """
 2 players: virus and medic
 virus player abilities:
-  invest in symptom hiding so the hosts get more turns before they show symptoms and can be detected by medic
+  invest in symptom hiding so the hosts get more turns before they show symptoms and can be detected by medic (max 6 turns, randomly so its not guaranteed that maxed out will grant 6 turn symptom hiding)
   invest in infections per turn (auto infect). each infected person will spread to 1 person next to them which can be increased to 4
-  invest in manual infections where he can choose (manual infect). can increase manual infections from 1 up to 4. *supposed to simulate*
-  spends virus points for in
+  invest in manual infections where he can choose (manual infect). can increase manual infections from 1 up to 4. *supposed to simulate transportation*
+  spends virus points for investments which are gained every turn for each infected individual which isn't caught by the medic
+medic player abilities: 
+  invest in more test kids per turn.
+  look at people with high symptoms (takes 3 turns to show by default) and isolate them with the cost of 1 med kit
+  invest in hospital capacity so that there can be more infected people before the game is over 
+  invest in vaccine speed so that the turns before winning as medic is reduced by 1 turn (starts at 50)
+  invest in social distancing: the next turn most people are separated by 1 grid unit to prevent spread (doesn't work if there are too many people)
+winning as medic:
+  vaccine is developed or all infected people are isolated
+winning as virus:
+  pass hospital capacity
+*every turn the people on the board will move 1 square unless social distancing is used that turn
 """
 from tkinter import *
 root=Tk()
